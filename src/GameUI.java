8f2f6022e29757141c7c70c79a9376cb8c922a6e
hyesung.park <48369800+hpark7@users.noreply.github.com>
2023-02-15 10:53:04 -0500

    Update GameUI.java

--- src/GameUI.java
+++ src/GameUI.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+//https://github.com/rhood4/Mini-Game1.git
 public class GameUI {
 
 
